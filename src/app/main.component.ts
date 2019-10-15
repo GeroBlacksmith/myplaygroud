194e892566cd1f0571e2037d073b2e93b112218a
GeroBlacksmith <gerardoherrera87@yahoo.com.ar>
2019-10-15 11:08:21 -0300

    Error message constant implementation

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from "@angular/router";
+
+
+
+import { errorMessages } from "./strings/error-messages";
 @Component({
 selector:'app-main',
-template:`<h1>Main Component</h1>
-<button (click)="o()">o</button>`
+template:`
+<h1>Main Component</h1>
+<div>
+  <button (click)="o()">navigate to pie</button>
+</div>
+<div>
+  <button (click)="showMessage()">Test Message</button>
+</div>
+  `,
+styles:[`
+button{ 
+  width:200px;
+  margin:10px;
+  border:none;
+  padding:10px;
+  transition:300ms all ease;
+  }
+button:hover{
+  background:#444;cursor:
+  pointer;color:white;
+  }
+`]
 })
 export class MainComponent{
   constructor(private router:Router){}
-o(){
-  // this.router.navigate(['pie',{b:"c",d:1}]);
-  this.router.navigate(['/pie'],{queryParams:{id:1, order:'popular'}});
-}
+  o(){
+    // this.router.navigate(['pie',{b:"c",d:1}]);
+    this.router.navigate(['/pie'],{queryParams:{id:1, order:'popular'}});
+  }
+  showMessage(){
+    console.error(errorMessages.testMessage)
+  }
 }
---
+++ src/app/strings/error-messages.ts
@@ -0,0 +1,3 @@
+export const errorMessages:{[s:string]:string}={
+  "testMessage":"This is a test message!"
+}
